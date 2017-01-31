4dd3b8fccb1b28ce6639682a5624182e66fa4d36
Ryan Scott <ryan.gl.scott@gmail.com>
2017-01-30 20:31:08 -0500

    Fix the non-Windows build

--- src/System/Console/MinTTY.hs
+++ src/System/Console/MinTTY.hs
@@ -19,7 +19,7 @@ the functions in this module will simply return 'False'.
 module System.Console.MinTTY (isMinTTY, isMinTTYHandle, HANDLE) where
 
 #if defined(WINDOWS)
-import System.Win32.Types (HANDLE)
+import           System.Win32.Types (HANDLE)
 
 # if MIN_VERSION_Win32(2,5,0)
 import qualified System.Win32.MinTTY as Win32 (isMinTTY, isMinTTYHandle)
@@ -29,27 +29,27 @@ import qualified System.Win32.MinTTY as Win32 (isMinTTY, isMinTTYHandle)
 import qualified System.Console.MinTTY.Win32 as Win32 (isMinTTY, isMinTTYHandle)
 # endif
 #else /* not Windows */
-import Foreign.Ptr (Ptr)
+import           Foreign.Ptr (Ptr)
 #endif
 
 -- | Returns 'True' if the current process's standard error is attached to a
 -- MinTTY console (e.g., Cygwin or MSYS). Returns 'False' otherwise.
 isMinTTY :: IO Bool
-isMinTTY = dispatch Win32.isMinTTY
+#if defined(WINDOWS)
+isMinTTY = Win32.isMinTTY
+#else
+isMinTTY = return False
+#endif
 
 -- | Returns 'True' is the given handle is attached to a MinTTY console
 -- (e.g., Cygwin or MSYS). Returns 'False' otherwise.
 isMinTTYHandle :: HANDLE -> IO Bool
-isMinTTYHandle = dispatch . Win32.isMinTTYHandle
+#if defined(WINDOWS)
+isMinTTYHandle = Win32.isMinTTYHandle
+#else
+isMinTTYHandle _ = return False
+#endif
 
 #if !defined(WINDOWS)
 type HANDLE = Ptr ()
-#endif
-
--- Do the right thing on Windows. Otherwise, just return False.
-dispatch :: IO Bool -> IO Bool
-#if defined(WINDOWS)
-dispatch x = x
-#else
-dispatch _ = return False
 #endif
